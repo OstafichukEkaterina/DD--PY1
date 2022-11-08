72e2a0b92bf2ca12428a7e397c811db30ab4cf6f
Ostafichuk Ekaterina <115481372+OstafichukEkaterina@users.noreply.github.com>
2022-11-08 17:33:49 +0300

    Лабораторная работа 4
    В таске 3 оставила код с предоставленным Вами набором print-ов

---
+++ Лабораторная работа 4/Task 2/main.py
@@ -0,0 +1,20 @@
+def get_count_char(str_):
+    count = 0
+    for letter in str_.lower():
+        if letter.isalpha():
+            dict_[letter] = dict_.get(letter, count) + 1
+    return dict_
+
+def percentage(dict_):
+    percentage_dict = {}
+    sum_ = sum(dict_.values())
+    for key, percent in dict_.items():
+        percentage_dict[key] = round(percent / sum_ * 100, 2)
+    return percentage_dict
+dict_ = {}
+main_str = """
+    Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
+print(get_count_char(main_str))
